edc36bfd104e0ad64c173aee9fe94980c6e297e0
ramana721 <venkataramana7210@gmail.com>
2025-04-01 22:52:15 +0530

    Event Handleres

---
+++ eventHandling.js
@@ -0,0 +1,48 @@
+var menuButton = document.getElementById("menuButton");
+var menu = document.getElementById("menu");
+// console.log(menuTrigger, menu);
+
+
+function showMenu(ele){
+    ele.style.display = "block";
+    console.log("It entered here");
+}
+
+function hideMenu(ele){
+    ele.style.display = "none";
+}
+
+function toggleMenu(ele){
+    var disp = ele.style.display;
+    if (disp === "none" || disp === ""){
+        ele.style.display = "block";
+    }
+    else if (disp === "block"){
+        ele.style.display = "none";
+    }
+}
+
+// menuButton.addEventListener("click", () => showMenu(menu));
+// menu.addEventListener("mouseleave", () => hideMenu(menu));
+
+// menuTrigger.addEventListener("mouseenter", showMenu);
+
+menuButton.addEventListener("click", () => toggleMenu(menu));
+
+var text = document.getElementById("text");
+// console.log(`textBefore: ${text}`)
+
+function validateInput(){
+    console.log("Key Pressed");
+}
+// function downKey(){
+
+// }
+// function upKey(){    
+//     console.log("Charer count:" + document.getElementById("text").value.length, text);
+// }
+
+text.addEventListener("keydown", (event) => console.log(`Key Down: ${event.key}`));
+text.addEventListener("keyup", (event) => console.log(`key Released: ${event.key}`));
+//text.addEventListener("keyup", () => console.log("Key Up"));
+
